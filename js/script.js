8b6c8ded304aee5ea08fad5ec73fbbba062875b4
Shifaa-Mahmoud <121222678+Shifaa-Mahmoud@users.noreply.github.com>
2023-06-08 13:40:50 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,7 @@
+var nav = document.getElementById("nav");
+        function nav()
+        {
+            document.getElementById("nav").style.background="silver";
+            document.getElementById("nav").style.color ="red";
+            document.getElementsByClassName("nav").style.backgroundcolor="oranged";
+        }
